5b37fa32ad439785f8990ed8c44dd569f5cd486f
Ilya <trutenilya@gmail.com>
2022-08-24 19:05:14 +0300

    Writing a programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] array = {"hello","2","world", ":-)"};
+
+
+
+int countOne = 0;
+for (int i = 0; i < array.Length; i++)
+{  
+   if (array[i].Length <= 3)
+   {
+     countOne++;
+   }
+}
+string [] newArray = new string[countOne];
+
+int countTwo = 0;
+
+for (int i = 0; i < array.Length; i++)
+{  
+   if (array[i].Length <= 3)
+   {
+     newArray[countTwo] = array[i];
+     countTwo++;
+   }
+}
+
+Console.WriteLine($"[{String.Join(", ", newArray)}]");
